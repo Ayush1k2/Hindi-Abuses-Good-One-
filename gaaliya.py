d783c9208bca4de74a21df0cc9786ed96eeee7ad
Amritanshu Shandilya <72188339+ShandilyaSolutions@users.noreply.github.com>
2022-11-17 09:49:30 +0000

    initial commit

---
+++ gaaliya.py
@@ -0,0 +1,49 @@
+" Personal Abuses "
+
+# Used to call someone abusively
+personal_gaali = [
+    "madarchod",
+    "behinchod",
+    "lodu",
+    "bhosdike",
+    "jhatu",
+    "jhat ke baal",
+    "randi ka bachaa",
+    "o jhaant ke baal",
+    "aye lodu lalit"
+]
+
+
+# Dedicated to Maataji of the victim
+maa_ki_gaali = [
+    "teri maiya randi",
+    "teri maa ki chut",
+    "tori mai ke chodo"
+]
+
+# For friends only
+frnds_ki_gali =[
+    "chutiya",
+    "lawde",
+    "lodchat",
+    "lund"
+]
+
+# Gali to be used with another item/gaali
+compound_gali =[
+    "tere gaand me",
+    "teri maa k chut me"
+]
+
+# Jawab dene waali gaali
+gaali_reply = [
+    "muh me le le mera",
+    "gaand mara tu",
+    "chala ja bhosdike",
+    "teri maa ka saaki naaka"
+]
+
+# For compound gaali
+items = [
+    "sariya"
+]
